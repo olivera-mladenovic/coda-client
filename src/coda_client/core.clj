b402abaf044f8b88d0839490a9d2ea3597d0f422
olivera-mladenovic <45879474+olivera-mladenovic@users.noreply.github.com>
2023-08-03 11:37:13 +0200

    fix: refactor make-api to receive params, err handlers

--- src/coda_client/core.clj
+++ src/coda_client/core.clj
@@ -3,50 +3,57 @@
             [clj-http.client :as http]
             [cheshire.core :as json]))
             
-(defn make-api-request [method endpoint config data]
-  (let [api-url (str (:base-url config) endpoint)
+(defn make-api-request [method endpoint api-key data params]
+  (let [api-url (str "https://coda.io/apis/v1" endpoint)
         body-data (if (nil? data)
                     nil
                     (json/generate-string data)
                     )]
-    (http/request 
-     {:method method :url api-url :body body-data :headers
-      {:Authorization (str "Bearer " (:api-key config))
-       :content-type "application/json"}})))
+    (http/request
+     {:method method :url api-url :body body-data :query-params params :throw-exceptions false :headers
+      {:Authorization (str "Bearer " api-key)
+       :content-type "application/json"}})
+    ))
 
-
-(defn list-docs [config]
+;; DOCS endpoints
+(defn list-docs [api-key params]
   (let [endpoint "/docs"
-        response (make-api-request :get endpoint config nil)]
+        response (make-api-request :get endpoint api-key nil params)]
     (if (= (:status response) 200)
       (json/parse-string (:body response) true)
-      (throw (ex-info "Failed to list docs"
+      (throw (ex-info (str "Failed to list docs."
+                           " Response: " (:body response))
                       {:status (:status response)
                        :body (:body response)})))))
 
-(defn create-doc [config data]
+(defn create-doc [api-key data]
   (let [endpoint "/docs"
-        response (make-api-request :post endpoint config data)]
+        response (make-api-request :post endpoint api-key data nil)]
     (if (= (:status response) 201)
       (json/parse-string (:body response) true)
-      (throw (ex-info "Failed to create doc"
+      (throw (ex-info (str "Failed to create doc."
+                           " Response: " (:body response))
                       {:status (:status response)
                        :body (:body response)})))))
 
-(defn delete-doc [config id]
+(defn delete-doc [api-key id]
   (let [endpoint (str "/docs/" id)
-        response (make-api-request :delete endpoint config nil)]
+        response (make-api-request :delete endpoint api-key nil nil)]
     (if (= (:status response) 202)
       true
-      (throw (ex-info "Failed to delete doc"
+      (throw (ex-info (str "Failed to delete doc."
+                           " Response: " (:body response))
                       {:status (:status response)
                        :body (:body response)})))))
 
-(defn get-doc [config id]
+(defn get-doc [api-key id]
   (let [endpoint (str "/docs/" id)
-        response (make-api-request :get endpoint config nil)]
+        response (make-api-request :get endpoint api-key nil nil)]
     (if (= (:status response) 200)
       (json/parse-string (:body response) true)
-      (throw (ex-info "Failed to get doc"
+      (throw (ex-info (str "Failed to get doc."
+                           " Response: " (:body response))
                       {:status (:status response)
                        :body (:body response)})))))
+
+;; PERMISSIONS endpoints
