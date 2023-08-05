c4541e60b24dfe709d992e9456b8ec5b4a68fa21
olivera-mladenovic <45879474+olivera-mladenovic@users.noreply.github.com>
2023-08-05 18:58:50 +0200

    chore: add permissions functions

--- src/coda_client/core.clj
+++ src/coda_client/core.clj
@@ -3,6 +3,7 @@
             [clj-http.client :as http]
             [cheshire.core :as json]))
             
+
 (defn make-api-request [method endpoint api-key data params]
   (let [api-url (str "https://coda.io/apis/v1" endpoint)
         body-data (if (nil? data)
@@ -57,3 +58,73 @@
                        :body (:body response)})))))
 
 ;; PERMISSIONS endpoints
+(defn get-sharing-metadata [api-key doc-id]
+  (let [endpoint (str "/docs/" doc-id "/acl/metadata")
+        response (make-api-request :get endpoint api-key nil nil)]
+    (if (= (:status response) 200)
+      (json/parse-string (:body response) true)
+      (throw (ex-info (str "Failed to get sharing metadata."
+                           " Response: " (:body response))
+                      {:status (:status response)
+                       :body (:body response)})))))
+
+(defn list-permissions [api-key doc-id params]
+  (let [endpoint (str "/docs/" doc-id "/acl/permissions")
+        response (make-api-request :get endpoint api-key nil params)]
+    (if (= (:status response) 200)
+      (json/parse-string (:body response) true)
+      (throw (ex-info (str "Failed to list permissions."
+                           " Response: " (:body response))
+                      {:status (:status response)
+                       :body (:body response)})))))
+
+(defn add-permission [api-key doc-id data]
+  (let [endpoint (str "/docs/" doc-id "/acl/permissions")
+        response (make-api-request :post endpoint api-key data nil)]
+    (if (= (:status response) 200)
+      (json/parse-string (:body response) true)
+      (throw (ex-info (str "Failed to add permission."
+                           " Response: " (:body response))
+                      {:status (:status response)
+                       :body (:body response)})))))
+
+(defn delete-permission [api-key doc-id permission-id]
+  (let [endpoint (str "/docs/" doc-id "/acl/permissions/" permission-id)
+        response (make-api-request :delete endpoint api-key nil nil)]
+    (if (= (:status response) 200)
+      true
+      (throw (ex-info (str "Failed to delete permission"
+                           " Response: " (:body response))
+                      {:status (:status response)
+                       :body (:body response)})))))
+
+(defn search-principals [api-key doc-id search-term]
+  (let [endpoint (str "/docs/" doc-id "/acl/principals/search")
+        response (make-api-request :get endpoint api-key nil {:query search-term})]
+    (if (= (:status response) 200)
+      (json/parse-string (:body response) true)
+      (throw (ex-info (str "Failed to search principals"
+                           " Response: " (:body response))
+                      {:status (:status response)
+                       :body (:body response)})))))
+
+(defn get-acl-settings [api-key doc-id]
+  (let [endpoint (str "/docs/" doc-id "/acl/settings")
+        response (make-api-request :get endpoint api-key nil nil)]
+    (if (= (:status response) 200)
+      (json/parse-string (:body response) true)
+      (throw (ex-info (str "Failed to get acl settings"
+                           " Response: " (:body response))
+                      {:status (:status response)
+                       :body (:body response)})))))
+
+
+(defn update-acl-settings [api-key doc-id settings]
+  (let [endpoint (str "/docs/" doc-id "/acl/settings")
+        response (make-api-request :patch endpoint api-key settings nil)]
+    (if (= (:status response) 200)
+      (json/parse-string (:body response) true)
+      (throw (ex-info (str "Failed to update acl settings"
+                           " Response: " (:body response))
+                      {:status (:status response)
+                       :body (:body response)})))))
